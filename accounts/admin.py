071222a647f1e7a7980d42a9f94d5df849c30afc
Xanazyki <Shmatko.mixail@mail.ru>
2026-05-14 23:08:32 +0300

    Conf model admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Profile, User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_staff", "is_active", "date_joined")
+    list_filter = ("is_staff", "is_active", "date_joined")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "display_name", "created_at")
+    search_fields = ("user__email", "display_name")
+    list_filter = ("created_at",)
--- wishlists/admin.py
+++ wishlists/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ItemComment, Wishlist, WishlistAccess, WishlistItem
+
+
+class WishlistItemInline(admin.TabularInline):
+    model = WishlistItem
+    extra = 0
+
+
+class WishlistAccessInline(admin.TabularInline):
+    model = WishlistAccess
+    extra = 0
+
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("title", "description", "owner__email")
+    inlines = [WishlistItemInline, WishlistAccessInline]
+
+
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "wishlist", "status", "priority", "approximate_price", "created_at")
+    list_filter = ("status", "priority", "created_at")
+    search_fields = ("title", "description", "wishlist__title")
+
+
+@admin.register(WishlistAccess)
+class WishlistAccessAdmin(admin.ModelAdmin):
+    list_display = ("wishlist", "user", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("wishlist__title", "user__email")
+
+
+@admin.register(ItemComment)
+class ItemCommentAdmin(admin.ModelAdmin):
+    list_display = ("item", "author", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("item__title", "author__email", "text")
