d7d8a791535784e31b0b88232975918b0a2e37e2
Alexandr Obuhov <sobuhov8@gmail.com>
2026-05-14 23:41:03 +0300

    добавил юрл в конфиг

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("accounts/", include("accounts.urls")),
+    path("wishlists/", include("wishlists.urls")),
 ]
